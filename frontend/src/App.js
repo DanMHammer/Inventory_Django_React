90d06d837f37f53e4efedf2c4380b6f70a90d67a
dan.hammer <daniel.m.hammer@gmail.com>
2019-07-30 13:33:18 -0500

    Added ObjectDisplay component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Container, Col, Row, ListGroup, ListGroupItem } from "reactstrap";
+import ObjectDisplay from "./components/ObjectDisplay";
 
 class App extends Component {
   state = {
@@ -21,23 +22,7 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <Container fluid>
-        <Row>
-          <Col sm={{ size: 8, offset: 2 }}>
-            {/*Object List*/}
-            <ListGroup>
-              {this.state.objects.map(object => (
-                <ListGroupItem key={object.id} className="list-group-item">
-                  <h1>{object.name}</h1>
-                  {object.description}
-                </ListGroupItem>
-              ))}
-            </ListGroup>
-          </Col>
-        </Row>
-      </Container>
-    );
+    return <ObjectDisplay objects={this.state.objects} />;
   }
 }
 
---
+++ frontend/src/components/ObjectDisplay.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Container, Col, Row, ListGroup, ListGroupItem } from "reactstrap";
+
+class ObjectDisplay extends React.Component {
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col sm={{ size: 8, offset: 2 }}>
+            {/*Object List*/}
+            <ListGroup>
+              {this.props.objects.map(object => (
+                <ListGroupItem key={object.id} className="list-group-item">
+                  <h1>{object.name}</h1>
+                  <p>{object.description}</p>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
